5b1c925a9bf2cbb22173437542e969071581ecbb
Jason Christian <jasonchristian462@gmail.com>
2024-05-07 20:55:18 +0700

    update

--- lib/firebase/firebase_options.dart
+++ lib/firebase/firebase_options.dart
@@ -4,13 +4,26 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
 class CustomFirebaseOptions {
   static FirebaseOptions get firebaseOptions => FirebaseOptions(
-    apiKey: 'AIzaSyBsD-Foc4X0uDOrlv47aFt1DZOiQsxjo98',
-    appId: '1:944733211622:android:4c0b14d752f87f0177e427',
-    messagingSenderId: '944733211622',
-    projectId: 'order-app-1341d',
-    authDomain: 'order-app-1341d.firebaseapp.com' ,
-    databaseURL: 'https://order-app-1341d-default-rtdb.asia-southeast1.firebasedatabase.app',
+    apiKey: 'AIzaSyBRMglzI8-IDm2eTZoIYg8F0jeFnjeTqxQ',
+    appId: '1:132157081374:android:cb6e3c4cc9b2fa73cfab0f',
+    messagingSenderId: '132157081374',
+    projectId: 'aplikasipemesanan-a60e7',
+    authDomain: 'aplikasipemesanan-a60e7.firebaseapp.com' ,
+    databaseURL: 'https://aplikasipemesanan-a60e7-default-rtdb.asia-southeast1.firebasedatabase.app',
     // storageBucket: 'order-app-1341d.appspot.com'
     // Tambahkan parameter lain jika diperlukan
   );
 }
+
+// class CustomFirebaseOptions {
+//   static FirebaseOptions get firebaseOptions => FirebaseOptions(
+//     apiKey: 'AIzaSyBsD-Foc4X0uDOrlv47aFt1DZOiQsxjo98',
+//     appId: '1:944733211622:android:4c0b14d752f87f0177e427',
+//     messagingSenderId: '944733211622',
+//     projectId: 'order-app-1341d',
+//     authDomain: 'order-app-1341d.firebaseapp.com' ,
+//     databaseURL: 'https://order-app-1341d-default-rtdb.asia-southeast1.firebasedatabase.app',
+//     // storageBucket: 'order-app-1341d.appspot.com'
+//     // Tambahkan parameter lain jika diperlukan
+//   );
+// }
